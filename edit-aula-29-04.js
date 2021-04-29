4c04439732f0130807e42b233b0a36b995116b10
allan02machado <70041162+allan02machado@users.noreply.github.com>
2021-04-29 20:47:33 -0300

    Material utilizado em aula do dia 29 abr

---
+++ edit-aula-29-04.js
@@ -0,0 +1,138 @@
+
+//====================================================================
+/*let soma = (a, b) => a + b;
+
+console.log(soma(25, 32));*/
+//====================================================================
+
+
+
+
+
+//====================================================================
+/*
+//let cumprimentar = function(cumprimento){
+//    console.log(`${cumprimento}, usuário!`);
+//};
+
+let usuario = {
+    nome: "Allan",
+    cumprimentar: function(cumprimento){
+        console.log(`${cumprimento}, ${this.nome}!`);
+    },
+};
+
+usuario.cumprimentar("Bom dia");
+usuario.nome = "Allan";
+usuario.cumprimentar("Boa noite");
+
+//cumprimentar("Bom dia!");
+//cumprimentar("Boa noite!");
+*/
+//====================================================================
+
+
+
+
+
+//====================================================================
+/*let aluno = {
+    nome: "Allan",
+    notas: [5, 7.5, 2.5, 0, "MB", false],
+};
+
+let alunos = [
+    {
+    nome: "Allan Cássio",
+    notas: [5, 7.5, 2.5, 0, "MB", false],
+    },
+    {
+    nome: "Allan Machado",
+    notas: [5, 7.5, 2.5, 0, "MB", false],
+    },
+    {
+    nome: "Allan de Oliveira",
+    notas: [5, 7.5, 2.5, 0, "MB", false],
+    }
+];
+
+console.log(alunos[1].notas[0]);*/
+//====================================================================
+
+
+
+
+
+//====================================================================
+/*let notas = [
+    7.5,
+    8,
+    6.5,
+    0,
+];
+
+//let indiceESCOLHIDO = 3
+
+console.log( notas[0] );*/
+//====================================================================
+
+
+
+
+
+//====================================================================
+/*let amigo = {
+    nome: "Joãozinho",
+    idade: 15
+};
+
+let inimigo = amigo;
+
+amigo.nome = "Bruxo";
+inimigo.idade = 18;
+
+console.log(amigo);
+console.log(inimigo);*/
+//====================================================================
+
+
+
+
+
+//====================================================================
+/*let amigo = "Joãozinho";
+let inimigo = amigo;
+
+amigo = "Bruxo";
+
+console.log(amigo);
+console.log(inimigo);*/
+//====================================================================
+
+
+
+
+
+//====================================================================
+/*let nome = "Allan";
+let idade = "22";
+let possuiCNH = true;
+
+console.log(nome);
+console.log(idade);
+console.log(possuiCNH);*/
+//====================================================================
+
+
+
+
+
+//====================================================================
+/*let usuario = {
+nome: "Allan",
+idade: 22,
+};
+
+console.log(usuario);
+console.log(usuario.nome);*/
+//====================================================================
